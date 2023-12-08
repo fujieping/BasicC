08439c566be5294f28dd50aad3ec7ea376fa58c4
挡泥板 <1573465644@qq.com>
2023-12-08 16:15:26 +0800

    [新增]动静数组

--- demoArray/demoArray.c
+++ demoArray/demoArray.c
@@ -26,14 +26,14 @@ int main()
     printf("%p\n", &array[0]);
     printf("%p\n", &array[1]);
     printf("%p\n", &array[9]);
-#if 0
+#if 0/* 进制*/
     /* 二进制*/
     /* 八进制*/
     /* 十进制*/
     /* 十六进制*/
     /* 0X34 = 3*16^1+4*16^0 */
 #endif
-#if 0
+#if 0/* 脏数据清除*/
     /* 清除脏数据*/
     memset(array, 0, sizeof(array));
     printf("================\n");
@@ -42,5 +42,11 @@ int main()
         printf("array[%d]=%d\n", idx, array[idx]);
     }
 #endif
+
+    /* 数组的越界访问*/
+    /* 判断索引的有效性*/
+    printf("array[-1]=%d\n",array[-1]);
+    printf("array[10]=%d\n",array[10]);
+    
     return 0;
 }
